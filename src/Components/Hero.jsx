ca1beb267a2a1df06b0a2715bea15df3f405227b
TakshaSharma <taksha.sharma08@gmail.com>
2024-06-10 22:17:09 +0530

    initial commit

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='border-b border-neutral-900 pb-4 lg:mb-35'>
+      <div className='flex flex-wrap'>
+        <div className='flex flex-col items-center lg:items-start'>
+            <h1>Taksha Sharma</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
